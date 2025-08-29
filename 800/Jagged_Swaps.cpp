fb8c1ee149ee554f46f5329057b58f458db1f0e3
Saumel-Das <saumel.spr@gmail.com>
2025-08-29 21:33:55 +0600

    Solved

---
+++ 800/Jagged_Swaps.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a,b) memset(a, b, sizeof(a))
+#define all(a) (a).begin(), (a).end()
+#define check cout << "yuhu" << endl
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &u : v) cin >> u;
+    if(v[0] == 1) yes;
+    else no;
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
