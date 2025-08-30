515f62c9739d5dd6fc82fb4a2ca2e04c690821a3
Saumel-Das <saumel.spr@gmail.com>
2025-08-30 21:39:14 +0600

    Solved

---
+++ 900/Longest_Divisors_Interval.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a,b) memset(a, b, sizeof(a))
+#define all(a) (a).begin(), (a).end()
+#define check cout << "yuhu" << endl
+
+
+void solve()
+{
+    ll n, cnt = 0, maxi = LLONG_MIN;
+    cin >> n;
+    for(int i = 1; i <= 100; i++)
+    {
+        if(n % i == 0) cnt++;
+        else {maxi = max(maxi, cnt); cnt = 0; }
+    }
+    cout << max(maxi, cnt) << endl;
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
