61f384c9f8848a253042ca0dc483128c2127c230
Saumel-Das <saumel.spr@gmail.com>
2025-08-31 11:38:45 +0600

    Solved

---
+++ 900/Balanced_Round.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a,b) memset(a, b, sizeof(a))
+#define all(a) (a).begin(), (a).end()
+#define check cout << "yuhu" << endl
+
+
+void solve()
+{
+    int n; cin >> n;
+    ll k; cin >> k;
+    vector<int> track;
+    vector<ll> v(n);
+    for(auto &u : v) cin >> u;
+
+    sort(v.rbegin(), v.rend());
+
+    int cnt = 1, ans = 0;
+    for(int i = 0; i < n - 1; i++)
+    {
+        if(v[i] - v[i + 1] <= k)
+        {
+            cnt++;
+        }
+        else
+        {
+            ans = max(ans, cnt);
+            cnt = 1;
+        }
+    }
+    ans = max(ans, cnt);
+    cout << n - ans << endl;
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
