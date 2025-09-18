46ba6a35e7ff3086af091da70ae57d9950690b80
Saumel-Das <saumel.spr@gmail.com>
2025-09-18 15:40:25 +0600

    Solved

---
+++ 800/Blank_Space.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a,b) memset(a, b, sizeof(a))
+#define check cout << "yuhu" << endl
+
+
+void solve()
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(auto &u : v) cin >> u;
+    int cnt = 0, ans = 0;
+    for(int i = 0; i < n; i++)
+    {
+        if(v[i] == 0)
+        {
+            cnt++;
+        }
+        else
+        {
+           ans = max(ans, cnt);
+           cnt = 0;
+        }
+    }
+    ans = max(ans, cnt);
+    cout << ans << endl;
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
