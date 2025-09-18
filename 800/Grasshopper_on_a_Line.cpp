bd3c75a074906dd0c3672f4bdeeff46c0af10317
Saumel-Das <saumel.spr@gmail.com>
2025-09-18 15:43:32 +0600

    Solved

---
+++ 800/Grasshopper_on_a_Line.cpp
@@ -0,0 +1,45 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+
+typedef long long ll;
+typedef double dl;
+
+#define endl '\n'
+#define yes cout << "YES" << endl
+#define no cout << "NO" << endl
+#define nl cout << endl
+#define mem(a, b) memset(a, b, sizeof(a))
+#define check cout << "-__________________-" << endl
+
+void solve()
+{
+    int n, k;
+    cin >> n >> k;
+    if (n % k)
+    {
+        cout << 1 << endl
+             << n << endl;
+    }
+    else
+    {
+        cout << 2 << endl << 1 << " " << n - 1 << endl;
+    }
+}
+int main()
+{
+    optimize();
+
+    int t = 1;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+
+    return 0;
+}
